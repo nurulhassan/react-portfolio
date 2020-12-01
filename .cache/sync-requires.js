2d8ae5aed798ac482e239923a7ea3d8de7871e6a
nurulhassan <nurulhassan2000@gmail.com>
2020-12-02 04:17:11 +0530

    Readme Updated

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
   "component---src-templates-post-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\src\\templates\\post.js"))),
   "component---src-templates-tag-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\src\\templates\\tag.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\src\\pages\\404.js"))),
   "component---src-pages-archive-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\src\\pages\\archive.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("G:\\Projects\\react-portfolio\\src\\pages\\index.js"))),
